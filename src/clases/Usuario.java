9d519f7361e057dcc018edf5765217610f947c05
IVANCCAMA <ccamaivan1@gmail.com>
2023-09-15 21:05:54 -0400

    + add in clases + modified in inicio.java and inicio.form

--- src/clases/Usuario.java
+++ src/clases/Usuario.java
@@ -11,11 +11,66 @@ package clases;
 public class Usuario {
     private int id;
     private String nombre;
-    private int ci;
-    private String cargo;
+    private String ci;
+    private String telefono;
     private String contraseña;
+    private String tipo;
+
+    public Usuario(int id, String nombre, String ci, String contraseña) {
+        this.id = id;
+        this.nombre = nombre;
+        this.ci = ci;
+        this.contraseña = contraseña;
+    }
     
-    public Usuario(){
+    public Usuario(){   
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCi(String ci) {
+        this.ci = ci;
+    }
+
+    public String getCi() {
+        return ci;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
     
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    public String getTipo() {
+        return tipo;
     }
 }
